8b9e23733dfd3b33ef6184c03e7cec1267cd5e0e
NurİpekTiryaki <ipek.tiryaki@extraloyalty.com>
2022-09-25 18:00:53 +0300

    Add project files.

---
+++ MongoDB101.API/Settings/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MongoDB101.API.Settings
+{
+    public class MongoDbSettings : IDbSettings
+    {
+        public string ConnectionString { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string Database { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string Collection { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
